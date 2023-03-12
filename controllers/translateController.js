c6238a586cfe469d7b3c4bb3681affd616869b85
Allain Woodsford <awoodsford@hotmail.com>
2023-03-13 09:42:56 +1100

    added commentry to troubleshoot

--- controllers/translateController.js
+++ controllers/translateController.js
@@ -11,11 +11,13 @@ const target = "ja";
 //This is a PATCH request
 const translateText = async (req,res) =>  {
   try {
+      console.log('attempt translate');
       const taskID = {id:req.body.id};
       //we are going to take the translated value, there is no point translating a task that is already in Japanese
       const foundTask = await taskQueries.getTask(taskID,['id','isTranslated','taskName']);
       if(foundTask != null)
       {
+        console.log('found task');
           //We should check if the task exists before we make an API call
           //Google API costs money don't want to make empty requests to translate.
           //Do a Guard for Null or empty text
@@ -27,22 +29,28 @@ const translateText = async (req,res) =>  {
             message: 'Empty Task'
           });
         } else {
+          console.log('task passes empty test');
             if(foundTask.isTranslated){
               //We've got a task we should check if its already translated
               res.status(400).json({
                 message: 'Task is already translated'
               });
             } else {
+              console.log('task isnt already translated');
               //At this point we have an untranslated task, that's not empty
                //We want to trim white spaces because they charge per character
                //this returns an array we just need 1 string
+               console.log('attempt translate');
               const translatedText =  await translate.translate(taskText,target);
+              console.log(tasktext + "/" + target);
               //Should be good at this point to make the PATCH request
               foundTask.set({
                 taskName:translatedText[0],
                 isTranslated:true
               });
+              console.log('attempt patch on task');
               await foundTask.save();
+              console.log('it went well');
               res.status(200).json({
                 code: 200,
                 message: 'task is now translated',
